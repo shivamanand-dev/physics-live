95f479578441603f0078bfc45f08e8c22f8500af
shivamanand-dev <shivam_anand@outlook.com>
2024-12-05 15:48:52 +0530

    basic structure

---
+++ src/component/Dropdown/index.js
@@ -0,0 +1,40 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+function Dropdown({ title = 'Chapter 1', questions = [] }) {
+  return (
+    <Menu as="div" className="w-full relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex justify-between w-full gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-inset ring-gray-300 hover:bg-gray-50 text-left my-1">
+          {title}
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="-mr-1 size-5 text-gray-400"
+          />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          {questions.map(e => {
+            return (
+              <MenuItem key={e.questionNo}>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                >
+                  {e.questionNo}
+                </a>
+              </MenuItem>
+            );
+          })}
+        </div>
+      </MenuItems>
+    </Menu>
+  );
+}
+
+export default Dropdown;
---
+++ src/component/HomePage/index.js
@@ -0,0 +1,190 @@
+/* eslint-disable no-unused-vars */
+'use client';
+
+import { useState } from 'react';
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  TransitionChild
+} from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import Dropdown from '../Dropdown';
+
+import class11 from '../../data/class11.json';
+
+function HomePage() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [activeChapter, setActiveChapter] = useState(0);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+
+  return (
+    <>
+      <Dialog
+        open={sidebarOpen}
+        onClose={setSidebarOpen}
+        className="relative z-50 lg:hidden"
+      >
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
+
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                </button>
+              </div>
+            </TransitionChild>
+            {/* Sidebar component, swap this element with another sidebar if you like */}
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+              <div className="flex h-16 shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="/img/logo.jpg"
+                  className="h-8 w-auto"
+                />
+              </div>
+              <nav className="flex flex-1 flex-col">
+                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                  <li>
+                    <div className="text-xs/6 font-semibold text-gray-400">
+                      Class 11
+                    </div>
+                    <ul role="list" className="-mx-2 space-y-1">
+                      {/* {class11.map(item => ({
+                         <li key={item.name}>
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-50 text-indigo-600'
+                                : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600',
+                              'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold'
+                            )}
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className={classNames(
+                                item.current
+                                  ? 'text-indigo-600'
+                                  : 'text-gray-400 group-hover:text-indigo-600',
+                                'size-6 shrink-0'
+                              )}
+                            />
+                            {item.name}
+                          </a>
+                        </li> 
+                      }))} */}
+                    </ul>
+                  </li>
+                  <li>
+                    <div className="text-xs/6 font-semibold text-gray-400">
+                      Class 12
+                    </div>
+                    {/* Class 12 */}
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+          <div className="flex h-16 shrink-0 items-center">
+            <img
+              alt="Your Company"
+              src="/img/logo.jpg"
+              className="h-8 w-auto"
+            />
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <div className="text-xs/6 font-semibold text-gray-400">
+                  Class 11
+                </div>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {class11.map((item, index) => {
+                    return (
+                      <li key={item.name}>
+                        <Dropdown
+                          title={`${item.name}`}
+                          questions={item.questions}
+                        />
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+              <li>
+                <div className="text-xs/6 font-semibold text-gray-400">
+                  Class 12
+                </div>
+                <ul role="list" className="-mx-2 mt-2 space-y-1">
+                  {/* Class 12 */}
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <button
+          type="button"
+          onClick={() => setSidebarOpen(true)}
+          className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+        >
+          <span className="sr-only">Open sidebar</span>
+          <Bars3Icon aria-hidden="true" className="size-6" />
+        </button>
+        <div className="flex-1 text-sm/6 font-semibold text-gray-900">
+          <img alt="Your Company" src="/img/logo.jpg" className="h-8 w-auto" />
+        </div>
+      </div>
+
+      <main className="lg:pl-72">
+        <div className="xl:pr-96">
+          <div className="p-12">
+            <h1 className="text-3xl font-semibold text-gray-900 pb-12">
+              {class11[activeChapter].questions[activeQuestion].title}
+            </h1>
+            <div
+              className="w-full relative px-4 py-10 sm:px-6 lg:px-8 lg:py-6"
+              style={{ paddingTop: '56.25%' }}
+            >
+              <iframe
+                className="rounded-lg absolute top-0 left-0 bottom-0 right-0 w-full h-full"
+                src="https://www.youtube.com/embed/EL-iAQekfz0"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen={true}
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block"></aside>
+    </>
+  );
+}
+
+export default HomePage;
